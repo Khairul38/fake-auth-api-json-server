c49702e425fc2f8d28e79e5c84037966b8e4013d
KHAIRUL ISLAM <khairulislam38@gmail.com>
2022-09-19 17:45:28 +0600

    add socket.io

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const auth = require("json-server-auth");
 const jsonServer = require("json-server");
 const express = require("express");
-const http = require("http")
+const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+global.io = io;
 
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -16,9 +19,9 @@ app.db = router.db;
 app.use(middlewares);
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
+  users: 640,
+  conversations: 660,
+  messages: 660,
 });
 
 app.use(rules);
